f9ee47f98cb1231a9eb65d829a172a5fccf09bc7
AnnLvu <Anasteishanakir@gmail.com>
2025-11-26 19:18:44 +0300

    update logger

--- observability/log/logger.ts
+++ observability/log/logger.ts
@@ -6,20 +6,15 @@ const logstashHost = process.env.LOGSTASH_HOST || "localhost";
 const logstashPort = parseInt(process.env.LOGSTASH_PORT || "5046");
 
 const logger = winston.createLogger({
-  defaultMeta: {
-    service: serviceName,
-    timestamp: new Date().toISOString(),
-  },
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: { service: "ft_transcendence" },
   transports: [
     // Console output
     new winston.transports.Console({
       format: winston.format.combine(
         winston.format.colorize(),
-        winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-        winston.format.printf(
-          ({ level, message, service, timestamp }) =>
-            `${timestamp} [${service}] ${level}: ${message}`
-        )
+        winston.format.simple()
       ),
     }),
 
